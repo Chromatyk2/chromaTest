0ee513c3d72550a178cf6e292871835fd7c243ce
PierreSchuvey <chromatykretro@gmail.com>
2023-03-06 15:33:44 +0100

    login

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -19,11 +19,11 @@ class AuthService {
   }
 
   async getUrlParams() {
-      return decodeQueryString(window.location.hash.slice(1));
+      return this.decodeQueryString(window.location.hash.slice(1));
   }
 
   isAuthenticated() {
-      const params = getUrlParams();
+      const params = this.getUrlParams();
       return params["access_token"] !== undefined;
   }
 
@@ -32,9 +32,9 @@ class AuthService {
           client_id: "401m5gmmyoy4jme9jo4n7bzz5zzt8t",
           redirect_uri: "https://chromatest.netlify.app/",
           response_type: "token",
-          scope: SCOPES.join(" "),
+          scope: [" "],
       };
-      const queryString = encodeQueryString(params);
+      const queryString = this.encodeQueryString(params);
       const authenticationUrl = `https://id.twitch.tv/oauth2/authorize?${queryString}`;
       window.location.href = authenticationUrl;
   }
