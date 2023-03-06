12e49f8eb5e2b101d01b3bf5d98b305a37ab6083
pierre schuvey <chromatykretro@gmail.com>
2023-03-06 19:49:37 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
-import { useCookies } from 'react-cookie';
+import { useCookies, getCooike } from 'react-cookie';
 import $ from "jquery";
 import './App.css';
 import 'react-tooltip/dist/react-tooltip.css'
@@ -11,7 +11,22 @@ import LaderBoard from './component/laderboard.js';
 import NavBar from './component/navbar.js';
 import Login from './services/auth.services.js';
 function App() {
-  return <Login />
+  console.log(getCookie("oauth"));
+  if(cookies.oauth) {
+    return <Login />
+  }
+  return(
+    <>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pokedex" element={<Pokedex />} />
+          <Route path="/leaderboard" element={<LaderBoard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
