9fc6fbd9bcd3338dba6bce0ad0077f3e14a6ef51
pierre schuvey <chromatykretro@gmail.com>
2023-03-06 22:38:32 +0100

    login

--- src/component/paginate.js
+++ src/component/paginate.js
@@ -11,7 +11,7 @@ function Items(props) {
   const shinys = pkmList.filter(item => item.shiny == 1);
   const nbShiny = shinys.length;
   const nbTotal = pkmList.length;
-  const token = cookies.oauth.params.access_token;
+  const token = cookies.oauth.params.id_token;
   const config = {
       headers: { Authorization: `Bearer ${token}` }
   };
