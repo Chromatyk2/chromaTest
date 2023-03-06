ab9f6b309f0782c5bd9ee6411c2e46b2e9bc9fe4
pierre schuvey <chromatykretro@gmail.com>
2023-03-07 00:49:16 +0100

    login

--- src/component/pokedex.js
+++ src/component/pokedex.js
@@ -1,4 +1,5 @@
 import React,{useState, useEffect} from 'react';
+import { Cookies, useCookies } from 'react-cookie';
 import Axios from 'axios'
 import '../App.css'
 import PkmList from './pkmList.js'
