8d996222887c54bc3a50eed10b9ad5f24a4593a7
pierre schuvey <chromatykretro@gmail.com>
2023-03-06 20:50:42 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
-import { instanceOf } from 'prop-types';
-import { withCookies, Cookies } from 'react-cookie';
+import { useCookies } from 'react-cookie';
 import $ from "jquery";
 import './App.css';
 import 'react-tooltip/dist/react-tooltip.css'
@@ -11,31 +10,24 @@ import BasicExample from './component/nav.js';
 import LaderBoard from './component/laderboard.js';
 import NavBar from './component/navbar.js';
 import Login from './services/auth.services.js';
-class App extends React.Component {
-      static propTypes = {
-        cookies: instanceOf(Cookies).isRequired
-      };
-     render() {
-     console.log(cookies);
-        return (
-          <>
-          {this.state.token == 0 ?
-            <Login/>
-            :
-            <>
-              <NavBar />
-              <BrowserRouter>
-                <Routes>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="/pokedex" element={<Pokedex />} />
-                  <Route path="/leaderboard" element={<LaderBoard />} />
-                </Routes>
-              </BrowserRouter>
-            </>
-          }
-          </>
-        )
-    }
+function App() {
+  const [cookies, setCookie] = useCookies(['oauth']);
+  console.log(cookies);
+  if(!cookies) {
+    return <Login />
+  }
+  return(
+    <>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pokedex" element={<Pokedex />} />
+          <Route path="/leaderboard" element={<LaderBoard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default withCookies(App);
+export default App;
