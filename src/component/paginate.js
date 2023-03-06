c9c939a3f536bcd1110c341cde4b34b81462fdaa
pierre schuvey <chromatykretro@gmail.com>
2023-03-07 00:34:53 +0100

    login

--- src/component/paginate.js
+++ src/component/paginate.js
@@ -11,8 +11,7 @@ function Items(props) {
   const shinys = pkmList.filter(item => item.shiny == 1);
   const nbShiny = shinys.length;
   const nbTotal = pkmList.length;
-  console.log(cookies);
-  const token = cookies;
+  const token = cookies.oauth.access_token;
   Axios.get(
   'https://id.twitch.tv/oauth2/userinfo',
   {
