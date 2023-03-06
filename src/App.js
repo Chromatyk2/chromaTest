215ccf24087ddadf99398ef01e56d5639ae0eab8
PierreSchuvey <chromatykretro@gmail.com>
2023-03-06 17:07:24 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, {useState, useEffect, Component, useContext, useMemo} from "react";
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import $ from "jquery";
 import './App.css';
@@ -9,108 +8,19 @@ import BasicExample from './component/nav.js';
 import LaderBoard from './component/laderboard.js';
 import NavBar from './component/navbar.js';
 function App() {
-  const [user, setUser] = useState(null);
-  // ID de l'application récupéré après l'avoir enregistrée
-  const CLIENT_ID = "401m5gmmyoy4jme9jo4n7bzz5zzt8t";
-
-  // Adresse où l'on veut que l'utilisateur soit redirigé après avoir autorisé
-  // l'application. Cette adresse DOIT être l'une de celles déclarées dans
-  // l'application sur dev.twitch.tv !!
-  const REDIRECT_URI = "https://chromatest.netlify.app/";
-
-  // Liste des éléments auxquels on souhaite accéder...  On reparlera de ça un
-  // peu plus tard ;)
-  const SCOPES = [];
-
-  // Diverses fonctions utilitaires
-  const helpers = {
-
-      // Encode un objet sous forme d'une querystring utilisable dans une URL :
-      // {"name": "Truc Muche", "foo": "bar"}  ->  "name=Truc+Muche&foo=bar"
-      encodeQueryString: function(params) {
-          const queryString = new URLSearchParams();
-          for (let paramName in params) {
-              queryString.append(paramName, params[paramName]);
-          }
-          return queryString.toString();
-      },
-
-      // Décode une querystring sous la forme d'un objet :
-      // "name=Truc+Muche&foo=bar"  ->  {"name": "Truc Muche", "foo": "bar"}
-      decodeQueryString: function(string) {
-          const params = {};
-          const queryString = new URLSearchParams(string);
-          for (let [paramName, value] of queryString) {
-              params[paramName] = value;
-          }
-          return params;
-      },
-
-      // Récupère et décode les paramètres de l'URL
-      getUrlParams: function() {
-          return helpers.decodeQueryString(window.location.hash.slice(1));
-      },
-
-  };
-
-  // Fonctions liées à Twitch
-  const twitch = {
-
-      // Vérifie si l'utilisateur est authentifié ou non
-      getUser: function() {
-          const params = helpers.getUrlParams();
-          fetch("https://api.twitch.tv/helix/users",
-            {
-              header: {
-                  Authorization: 'Bearer ' + params["access_token"]
-              }
-            }
-          )
-          .then(res => res.json())
-          .then(
-            (result) => {
-              setUser(result);
-            }
-          )
-      },
-      // Redirige l'utilisateur sur la page d'authentification de Twitch avec les
-      // bons paramètres
-      authentication: function() {
-          const params = {
-              client_id: CLIENT_ID,
-              redirect_uri: REDIRECT_URI,
-              response_type: "token",
-              scope: SCOPES.join(" "),
-          };
-          const queryString = helpers.encodeQueryString(params);
-          const authenticationUrl = `https://id.twitch.tv/oauth2/authorize?${queryString}`;
-          window.location.href = authenticationUrl;
-      },
-
-  };
-  useEffect(() => {
-   twitch.getUser();
- }, [])
-// Fonction principale
-console.log(user);
-  return (
+  return(
     <>
-      {user.status == 401 ?
-        <button onClick={twitch.authentication}>Connexion !</button>
-      :
-        <>
-          <NavBar />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/pokedex" element={<Pokedex />} />
-              <Route path="/leaderboard" element={<LaderBoard />} />
-            </Routes>
-          </BrowserRouter>
-        </>
-      }
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Main />} />
+          <Route path="/pokedex" element={<Pokedex />} />
+          <Route path="/leaderboard" element={<LaderBoard />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
