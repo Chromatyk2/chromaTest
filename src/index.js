c86627aed33e9396cfd7783f56ba0f89622446f4
pierre schuvey <chromatykretro@gmail.com>
2023-03-06 18:58:03 +0100

    login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useCookies } from 'react-cookie';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
