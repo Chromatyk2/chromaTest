96099c133b632fc7aeb6a79b89015051ad60fc72
pierre schuvey <chromatykretro@gmail.com>
2023-03-06 18:28:54 +0100

    login

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -4,17 +4,14 @@ function AuthService() {
   const CLIENT_ID = "401m5gmmyoy4jme9jo4n7bzz5zzt8t";
   const REDIRECT_URI = "https://chromatest.netlify.app/";
   const SCOPES = [];
-
-  encodeQueryString(params) {
+  const encodeQueryString = (params) => {
       const queryString = new URLSearchParams();
       for (let paramName in params) {
           queryString.append(paramName, params[paramName]);
       }
       return queryString.toString();
-  }
-
-
-  authentication() {
+  };
+  const authentication = () => {
     const params = {
         client_id: CLIENT_ID,
         redirect_uri: REDIRECT_URI,
@@ -24,22 +21,20 @@ function AuthService() {
       const queryString = encodeQueryString(params);
       const authenticationUrl = `https://id.twitch.tv/oauth2/authorize?${queryString}`;
       window.location.href = authenticationUrl;
-  }
-
-  decodeQueryString(string) {
+  };
+  const decodeQueryString = (string) => {
       const params = {};
       const queryString = new URLSearchParams(string);
       for (let [paramName, value] of queryString) {
           params[paramName] = value;
       }
       return params;
-  }
-
-  getUrlParams() {
+  };
+  const getUrlParams = () => {
       return this.decodeQueryString(window.location.hash.slice(1));
-  }
+  };
 
-  isAuthenticated() {
+  const isAuthenticated = () => {
       const params = this.getUrlParams();
       return params["access_token"] !== undefined;
   }
