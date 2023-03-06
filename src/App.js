d411e0d54385559583db708563878e11c6edc3bf
pierre schuvey <chromatykretro@gmail.com>
2023-03-06 20:26:01 +0100

    login

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ class App extends React.Component {
           token: cookies.get('oauth')
         };
       }
-      console.log(this.state.token);
      render() {
         return (
           <>
