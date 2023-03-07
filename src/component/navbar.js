317b2ea4cf238289adf09663eb92116729a124e7
PierreSchuvey <chromatykretro@gmail.com>
2023-03-07 16:21:10 +0100

    user cookie

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from "react-router-dom";
+import {BrowserRouter, Link} from "react-router-dom";
 
 function NavBar() {
   return (
@@ -11,9 +11,9 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Accueil</Nav.Link>
-            <Nav.Link as={Link} to="/pokedex">Pokedex</Nav.Link>
-            <Nav.Link as={Link} to="/leaderboard">Classement</Nav.Link>
+            <Link to="/">Accueil</Link>
+            <Link to="/pokedex">Pokedex</Link>
+            <Link to="/leaderboard">Classement</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
