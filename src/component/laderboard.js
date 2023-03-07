9ce5c1d17e0d74c18ee01f0ad20b22995e977c0f
PierreSchuvey <chromatykretro@gmail.com>
2023-03-07 13:42:34 +0100

    login

--- src/component/laderboard.js
+++ src/component/laderboard.js
@@ -86,8 +86,18 @@ function LaderBoard() {
                 <div class="score">
                   {topThree[0].nbCapture}
                 </div>
-              </div><div class="itemTwo">
+              </div>
+              <div class="itemTwo">
                 <div class="pic picTwo"></div>
+                <div class="name">
+                  {topThree[1].pseudo}
+                </div>
+                <div class="score">
+                  {topThree[1].nbCapture}
+                </div>
+              </div>
+              <div class="itemThree">
+                <div class="pic picThree"></div>
                 <div class="name">
                   {topThree[1].pseudo}
                 </div>
