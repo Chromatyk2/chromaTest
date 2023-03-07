e7885b30270c30d1a22e52aa6b6f07d496800c31
PierreSchuvey <chromatykretro@gmail.com>
2023-03-07 16:12:58 +0100

    user cookie

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -11,7 +11,7 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Accueil</Navbar.Brand>
+            <Nav.Link as={Link} to="/">Accueil</Nav.Link>
             <Nav.Link as={Link} to="/pokedex">Pokedex</Nav.Link>
             <Nav.Link as={Link} to="/leaderboard">Classement</Nav.Link>
           </Nav>
