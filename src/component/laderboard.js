437a5365b98d07797de9032143410bf1fa3fce75
PierreSchuvey <chromatykretro@gmail.com>
2023-03-07 15:32:00 +0100

    login

--- src/component/laderboard.js
+++ src/component/laderboard.js
@@ -112,8 +112,18 @@ function LaderBoard(props) {
         <div class="list">
         {others.length > 0 &&
           others.map((val, key) => {
-          return pseudo == val.pseudo
+          return pseudo == val.pseudo ?
             <div className="item myItem">
+              <div className="pic"><p>#{key + 4}</p></div>
+              <div className="name">
+                {val.pseudo}
+              </div>
+              <div className="score">
+                {val.nbCapture}
+              </div>
+            </div>
+            :
+            <div className="item">
               <div className="pic"><p>#{key + 4}</p></div>
               <div className="name">
                 {val.pseudo}
