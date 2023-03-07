4db4fb79d09771c03563bcc872667021a00f078f
PierreSchuvey <chromatykretro@gmail.com>
2023-03-07 11:07:52 +0100

    login

--- src/component/home.js
+++ src/component/home.js
@@ -6,12 +6,14 @@ import PkmList from './pkmList.js'
 function HomePage(props) {
   return (
     <>
-      <p className="myNetworks">Mes reseaux</p>
-      <div className="social">
-        <a className="socialLink" target='_blank' href="https://discord.gg/8V6fyQdSCG"><i class="fa-brands fa-discord"></i>Discord</a>
-        <a className="socialLink" target='_blank' href="https://twitch.tv/chromatyk"><i class="fa-brands fa-twitch"></i>Twitch</a>
-        <a className="socialLink" target='_blank' href="https://twitter.com/Chromatyk_"><i class="fa-brands fa-twitter"></i>Twitter</a>
-        <a className="socialLink" target='_blank' href="https://www.instagram.com/chromatyk_/"><i class="fa-brands fa-instagram"></i>Instagram</a>
+      <div className="socialContainer">
+        <p className="myNetworks">Mes reseaux</p>
+        <div className="social">
+          <a className="socialLink" target='_blank' href="https://discord.gg/8V6fyQdSCG"><i class="fa-brands fa-discord"></i>Discord</a>
+          <a className="socialLink" target='_blank' href="https://twitch.tv/chromatyk"><i class="fa-brands fa-twitch"></i>Twitch</a>
+          <a className="socialLink" target='_blank' href="https://twitter.com/Chromatyk_"><i class="fa-brands fa-twitter"></i>Twitter</a>
+          <a className="socialLink" target='_blank' href="https://www.instagram.com/chromatyk_/"><i class="fa-brands fa-instagram"></i>Instagram</a>
+        </div>
       </div>
     </>
   )
