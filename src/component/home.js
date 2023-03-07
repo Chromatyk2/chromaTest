5c142a089462bdd41fb7b7f7c8be6abf8cf65817
PierreSchuvey <chromatykretro@gmail.com>
2023-03-07 10:41:25 +0100

    login

--- src/component/home.js
+++ src/component/home.js
@@ -6,11 +6,12 @@ import PkmList from './pkmList.js'
 function HomePage(props) {
   return (
     <>
+      <p className="myNetworks">Mes réseaux</p>
       <div className="social">
-        <a target='_blank' href="https://discord.gg/8V6fyQdSCG"><i class="fa-brands fa-discord"></i></a>
-        <a target='_blank' href="https://twitch.tv/chromatyk"><i class="fa-brands fa-twitch"></i></a>
-        <a target='_blank' href="https://twitter.com/Chromatyk_"><i class="fa-brands fa-twitter"></i></a>
-        <a target='_blank' href="https://www.instagram.com/chromatyk_/"><i class="fa-brands fa-instagram"></i></a>
+        <a className="socialLink" target='_blank' href="https://discord.gg/8V6fyQdSCG"><i class="fa-brands fa-discord"></i></a>
+        <a className="socialLink" target='_blank' href="https://twitch.tv/chromatyk"><i class="fa-brands fa-twitch"></i></a>
+        <a className="socialLink" target='_blank' href="https://twitter.com/Chromatyk_"><i class="fa-brands fa-twitter"></i></a>
+        <a className="socialLink" target='_blank' href="https://www.instagram.com/chromatyk_/"><i class="fa-brands fa-instagram"></i></a>
       </div>
     </>
   )
