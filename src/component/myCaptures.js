1f733cc8295211f3d48c0ca2dc90d5195851047e
pierre schuvey <chromatykretro@gmail.com>
2023-03-12 11:36:20 +0100

    test

--- src/component/myCaptures.js
+++ src/component/myCaptures.js
@@ -4,20 +4,28 @@ import ReactPaginate from 'react-paginate';
 import Axios from 'axios'
 import Pagination from './paginate.js';
 import '../App.css'
+import moment from 'moment';
 
 function MyCaptures(props) {
-console.log(props.captures);
-  return (
-    <>
-    <p>Vous avez capturé {props.captures.length} fois :</p>
-      {props.captures.map((val, key) => {
-        return (
-          <>
-            <p>{val.dateCapture}</p>
-          </>
-        )
-      })}
-    </>
-  );
+  if(props.captures.length > 0){
+    return (
+      <>
+        <p className="titleMyCaptures">Vous avez capturé {props.captures.length} fois :</p>
+        {props.captures.map((val, key) => {
+          return (
+            <>
+              <p>{val.pkmName+" - "+moment(val.dateCapture).utc().format('DD/MM/YYYY')}</p>
+            </>
+          )
+        })}
+      </>
+    );
+  }else{
+    return (
+      <>
+        <p className="titleMyCaptures">Vous n'avez jamais capturé ce Pokemon</p>
+      </>
+    );
+  }
 }
 export default MyCaptures
