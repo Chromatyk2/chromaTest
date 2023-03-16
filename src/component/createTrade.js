22e67f1c7a856db0c650471866a98eb62de9e99e
PierreSchuvey <chromatykretro@gmail.com>
2023-03-16 15:02:20 +0100

    fix

--- src/component/createTrade.js
+++ src/component/createTrade.js
@@ -27,6 +27,10 @@ const [tradeDone, setTradeDone] = useState(false);
       idMainCapture:idMainCapture,
       idSecondCapture:null,
       state:1
+    },{
+      headers: {
+        'Access-Control-Allow-Origin' : '*'
+      }
     }
     ).then(
       (result) => {
