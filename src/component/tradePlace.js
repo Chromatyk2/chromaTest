8fae6efdb44de8cfd4c21d80770bfeb67510d9f3
PierreSchuvey <chromatykretro@gmail.com>
2023-03-17 16:41:23 +0100

    fix

--- src/component/tradePlace.js
+++ src/component/tradePlace.js
@@ -7,14 +7,36 @@ import '../App.css'
 
 function TradePlace(props) {
 const pseudo = props.cookies.user.data[0].login;
-   return (
-     <>
-      <div className="tradePlaceContainer">
-        <p className="titleMyCaptures">Echanges</p>
-        <div className="myTradesContainer">
-        </div>
-      </div>
-     </>
-   );
+  const [myTrades, setMyTrades] = useState([]);
+  const [disable, setDisable] = useState(false);
+  useEffect(() => {
+     Axios.get('/api/getAllTrades/'+props.pseudo)
+     .then(function(response){
+        setMyTrades(response.data);
+      })
+  }, [])
+
+  if(myTrades.length > 0){
+    return(
+      <>
+        {myTrades.map((val, key) => {
+            return (
+              <>
+              <div className="uniqueTradeContainer">
+                <img src={val.pkmImage}></img>
+                <p className="pokemonNameTrade">{val.pkmName}</p>
+                <button value={val.tradeId} className="deleteTrade" onClick={deleteTrade} disabled={disable}>{disable === false ? "Annuler" : "Traitement"}</button>
+              </div>
+              </>
+            )
+            })
+        }
+      </>
+    )
+  }else{
+      return(
+        <p>Pas de pokemon en echange</p>
+      )
+    }
 }
 export default TradePlace
