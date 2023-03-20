592beb49d548fe2554801b81da16274298afa91a
pierre schuvey <chromatykretro@gmail.com>
2023-03-20 16:28:31 +0100

    guess

--- src/component/guessTrade.js
+++ src/component/guessTrade.js
@@ -70,7 +70,7 @@ console.log(items);
                   onHover={handleOnHover}
                   onSelect={handleOnSelect}
                   onFocus={handleOnFocus}
-                  autoFocus
+                  autoFocus=true
                   formatResult={formatResult}
                   styling={
                     {
