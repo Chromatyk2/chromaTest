44ef859cc27e190e8d890b45234b79397e58bfca
pierre schuvey <chromatykretro@gmail.com>
2023-03-20 15:55:55 +0100

    guess

--- src/component/guessTrade.js
+++ src/component/guessTrade.js
@@ -75,10 +75,21 @@ console.log(items);
                   formatResult={formatResult}
                   styling={
                     {
-                      backgroundColor: "black",
-                      display:"flex",
-                      flexFlow:"column"
-                    }
+                       height: "44px",
+                       border: "1px solid #dfe1e5",
+                       borderRadius: "24px",
+                       backgroundColor: "white",
+                       boxShadow: "rgba(32, 33, 36, 0.28) 0px 1px 6px 0px",
+                       hoverBackgroundColor: "#eee",
+                       color: "#212121",
+                       fontSize: "16px",
+                       fontFamily: "Arial",
+                       iconColor: "grey",
+                       lineColor: "rgb(232, 234, 237)",
+                       placeholderColor: "grey",
+                       clearIconMargin: '3px 14px 0 0',
+                       searchIconMargin: '0 0 0 16px'
+                     }
                   }
                 />
               </div>
