973bd9384b54ba0b6405a9618b1da9ed5d0eb4ac
PierreSchuvey <chromatykretro@gmail.com>
2023-03-22 17:33:12 +0100

    fix

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -8,7 +8,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import {BrowserRouter, Link} from "react-router-dom";
 
 function NavBar(props) {
-  const MINUTE_MS = 10;
+  const MINUTE_MS = 15000;
   const [count, setCount] = useState(0);
   const [pseudo, setPseudo] = useState('');
   useEffect(() => {
