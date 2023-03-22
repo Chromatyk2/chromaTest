efc1604841335260ccb629a7162271b392975423
PierreSchuvey <chromatykretro@gmail.com>
2023-03-22 17:20:02 +0100

    fix

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -14,6 +14,7 @@ function NavBar(props) {
   useEffect(() => {
     const interval = setInterval(() => {
       if(props.cookies.user !== undefined){
+        console.log(props.cookies.user);
         setPseudo(props.cookies.user.data[0].login);
       }
       Axios
