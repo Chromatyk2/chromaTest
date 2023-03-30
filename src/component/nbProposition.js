a983e37fceb3fed2a03d778f571079bebd14dd49
PierreSchuvey <chromatykretro@gmail.com>
2023-03-30 12:00:24 +0200

    show guess

--- src/component/nbProposition.js
+++ src/component/nbProposition.js
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams,BrowserRouter, Link } from 'react-router-dom'
 import ReactPaginate from 'react-paginate';
 import Axios from 'axios'
 import Pagination from './paginate.js';
