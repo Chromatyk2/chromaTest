4e81c0846bb2eaa0f02b03018f8bb171b89b6c2b
PierreSchuvey <chromatykretro@gmail.com>
2023-03-30 15:42:43 +0200

    fix

--- src/component/laderboard.js
+++ src/component/laderboard.js
@@ -92,24 +92,28 @@ function LaderBoard(props) {
                   {topThree[0].nbCapture}
                 </div>
               </div>
-              <div class="itemTwo">
-                <div class="pic picTwo"></div>
-                <div class="name">
-                  {topThree[1].pseudo}
+              {topThree.length > 1 &&
+                <div class="itemTwo">
+                  <div class="pic picTwo"></div>
+                  <div class="name">
+                    {topThree[1].pseudo}
+                  </div>
+                  <div class="score">
+                    {topThree[1].nbCapture}
+                  </div>
                 </div>
-                <div class="score">
-                  {topThree[1].nbCapture}
+              }
+              {topThree.length > 2 &&
+                <div class="itemThree">
+                  <div class="pic picThree"></div>
+                  <div class="name">
+                    {topThree[2].pseudo}
+                  </div>
+                  <div class="score">
+                    {topThree[2].nbCapture}
+                  </div>
                 </div>
-              </div>
-              <div class="itemThree">
-                <div class="pic picThree"></div>
-                <div class="name">
-                  {topThree[2].pseudo}
-                </div>
-                <div class="score">
-                  {topThree[2].nbCapture}
-                </div>
-              </div>
+              }
             </div>
           </>
         }
